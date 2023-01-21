1117e8b89a771b162fbb5bc70ad8b8792edd0ed7
Ken Wang <kenuiuc@gmail.com>
2023-01-21 11:16:52 -0800

    add a dummy tests file

--- R/num_imputer.R
+++ R/num_imputer.R
@@ -1,12 +1,15 @@
-#' Imputation function for a numerical column 
+#' Imputation function for a numerical column
 #'
-#' @param x input data tibble 
+#' @param x input data tibble
 #' @param col name of the column to modify
 #' @param method name of the imputing strategy. They include ['knn','quant_rand','mean','median','mode','bayes'], the default method is knn.
 #'
 #' @return output data tibble after imputation
-#'
-#' @examples num_imputer(input_tibble, col_name, "mean")
+#' @export
+#' @examples
+#' a <- 1:5
+#' input_tibble <- data.frame(a, a * 2)
+#' num_imputer(input_tibble, "col_name", "mean")
 num_imputer <- function(x, col, method) {
   x
 }
