e7caeaf2d819260124e16747f4879e48c53ce0d1
renee-kwon <112587916+renee-kwon@users.noreply.github.com>
2023-01-21 19:13:21 -0800

    add docstring for cat_imputer

--- R/cat_imputer.R
+++ R/cat_imputer.R
@@ -1,3 +1,14 @@
-cat_imputer <- function(x, col, method) {
+#' Imputation function for a categorical column
+#'
+#' @param x input data tibble
+#' @param col name of the column to modify
+#' @param method name of the imputing strategy. They include ['knn', 'rand', 'frequent', and 'bayes'].
+#'
+#' @return output data tibble after imputation
+#' @export
+#' @examples
+#' input_df <- data.frame(a = c('BC', 'BC', 'ON', 'AB', NaN))
+#' bol_imputer(input_df, "a", "knn")
+bol_imputer <- function(x, col, method) {
   x
 }
