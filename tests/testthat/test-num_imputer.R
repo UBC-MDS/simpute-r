4af0f4d33d12a31c426a999beaf51851eca107ff
althrun <althrunsun@gmail.com>
2023-01-29 09:36:15 -0800

    creaed teset numeric data set

--- tests/testthat/test-num_imputer.R
+++ tests/testthat/test-num_imputer.R
@@ -1,3 +1,12 @@
+df_with_na <- data.frame(continent = (c("North America", "Europe", "Australia", "Australia", "Asia", "Asia", "Asia", "Asia")),
+                         ranking = c(NA, "2", "2", "3", "3", "3", "4", "4"),
+                         date = c("4/2/2013", "4/2/2014", "4/2/2015", "4/2/2016", "4/2/2017", "4/2/2018", "4/2/2019", "4/2/2020"))
+
+df_no_na <- data.frame(continent = (c("North America", "Europe", "Australia", "Australia", "Asia", "Asia", "Asia", "Asia")),
+                       ranking = c("2", "2", "2", "3", "3", "3", "4", "4"),
+                       date = c("4/2/2013", "4/2/2014", "4/2/2015", "4/2/2016", "4/2/2017", "4/2/2018", "4/2/2019", "4/2/2020"))
+
+library(testthat)
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
