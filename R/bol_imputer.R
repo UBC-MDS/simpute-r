e15be8a5298c2d6f5ba71e2dd5f6c3fd370e48de
LisaSeq <112457048+LisaSeq@users.noreply.github.com>
2023-01-29 00:52:11 -0800

    Finished script for bol_imputer

--- R/bol_imputer.R
+++ R/bol_imputer.R
@@ -1,7 +1,7 @@
 #' Imputation function for a boolean column
 #'
 #' @param x input data tibble
-#' @param col name of the column to modify
+#' @param c name of the logical class or boolean column to modify
 #' @param method name of the imputing strategy. They include ['mode'].
 #'
 #' @return output data tibble after imputation
@@ -13,47 +13,45 @@ library(dplyr)
 library(readr)
 library(tidyverse)
 
-bol_imputer <- function(df, col, method) {
-  {
+bol_imputer <- function(x, c, method = 'mode') {
     # check the input dataframe is a dataframe
-    if (!is.data.frame(df)) {
-      stop("The value of the argument 'dataframe' should be of type  'data.frame' or 'tibble'.")
+    if (!is.data.frame(x)) {
+      stop("The argument 'dataframe' should be of type 'data.frame' or 'tibble'.")
     }
-    # check column exists in dataframe
-    if (col %in% colnames(df) == FALSE) {
-      stop("The given column name must exist in the given dataframe.")
-    }
+    
     # check dataframe contains data
-    if (nrow(df{{col}}) == 0) {
+    if (nrow(x) == 0) {
       stop("The dataframe must have at least 1 row.")
     }
-    
+  
+    # check column exists in dataframe
+    if (c %in% colnames(x) == FALSE) {
+      stop("The given column name must exist in the given dataframe.")
+    }
+  
     # Change any empty string values to NA
-    df <- df |> mutate_all(na_if,"")
+    x <- x |> dplyr::mutate_all(na_if,"")
     
-    # check there are NA's in the column
-    if (sum(is.na(df{{col}})) == 0) {
+    # Check there are missing values in the column
+    if (sum(is.na(x[[c]])) == 0) {
       stop("There is no missing data in the specified column.")
     }
     
-    #Check if column contains boolean values
-    if (all(df{{col}} %in% c(TRUE, FALSE, T, F, 0, 1), na.rm = TRUE) == "FALSE"){
-      stop("This column is not a boolean column.")
-    }
+    #Check if column contains boolean values 
+    if (class(x[[c]]) != "logical") {
+    stop("This column is not a boolean column.")
+      }
     
-    #Or 
-    #if (class(df{{col}}) != "logical") {
-    #stop("This column is not a boolean column.")}
-    
-    #Turn column into logical values
-    
+    true_val <- x |> filter(c == TRUE) |> nrow()
+    false_val <- x |> filter(c == FALSE) |> nrow()
     
     # find most frequent value and impute
-    if (summary(df{{col}})[['TRUE']] > summary(df{{col}})[['FALSE']]) {
-      df <- df |> mutate({{col}} = replace_na({{col}}, T))
+    if (true_val >= false_val) {
+      x[[c]][is.na(x[[c]])] <- TRUE
     } else {
-      df <- df |> mutate({{col}} = replace_na({{col}}, F))
+      x[[c]][is.na(x[[c]])] <- FALSE
     }
     
-    return(df)
-  }}
+    return(x)
+  }
+
