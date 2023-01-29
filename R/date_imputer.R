6afeeff36ac401ee4ac52be518a76e2178c4bd92
Ken Wang <kenuiuc@gmail.com>
2023-01-29 10:01:10 -0800

    add date_imputer implementation and tests

--- R/date_imputer.R
+++ R/date_imputer.R
@@ -1,3 +1,6 @@
+library(tidyverse)
+library(lubridate) 
+
 #' Imputation function for a date column
 #'
 #' @param x input data tibble
@@ -7,8 +10,10 @@
 #' @return output data tibble after imputation
 #' @export
 #' @examples
-#' input_df <- data.frame(a = (c(as.Date('1990/02/17'), as.Date('1991/02/17'), NaN, as.Date('1992/02/17'))))
+#' input_df <- data.frame(a = (c(as.Date('1990/02/17'), as.Date('1991/02/17'), NA, as.Date('1992/02/17'))))
 #' date_imputer(input_df, "a", "median")
-date_imputer <- function(x, col, method) {
+date_imputer <- function(x, col, method = "median") {
+  # fill the NA cells with a hardcoded date `01/29/2023`
+  x[, col] <- x[, col] |> tidyr::replace_na(lubridate::as_date("01/29/2023", format = "%m/%d/%Y"))
   x
 }
